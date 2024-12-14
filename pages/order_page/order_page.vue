5cdd0b828c64825c064587512620a6f489ce7229
“zxl” <1650030420@qq.com>
2024-12-15 02:01:12 +0800

    fix: fix

--- pages/order_page/order_page.vue
+++ pages/order_page/order_page.vue
@@ -3,8 +3,16 @@
 	<view>
 		<view class="order_container">
 			<!-- 日期时间展示 -->
-			<map id="myMap" style="width: 100%; height: 300px;" :latitude="latitude" :longitude="longitude"
-				:markers="markers" show-location @tap="chooseLocationInfo()"></map>
+			<view class="map-area">
+				<map id="myMap" v-if="latitude" style="width: 100%; height: 300px;" :latitude="latitude" :longitude="longitude"
+					:markers="markers" min-scale="10" @markertap="handleTapMarker" @tap="handleTapMap"></map>
+				<view class="current-hospital" v-if="!!currentHospital && showCurrentHospital">
+					<view class="name">{{currentHospital.hospitalName}}</view>
+					<view class="tags" v-if="currentHospital.tags.length">
+						<view class="tag" v-for="(item,index) in currentHospital.tags" :key="index">{{item}}</view>
+					</view>
+				</view>
+			</view>
 			<!-- start -->
 			<div class="form_container">
 				<u--form labelPosition="left" :model="formData" ref="form">
@@ -20,6 +28,12 @@
 					<u-form-item label="年龄" prop="userAge" borderBottom label-position="top" label-width="100%"
 						style="margin-top: 8px;">
 						<u-input v-model="formData.userAge" border="none" placeholder="请输入年龄" type="number"></u-input>
+					</u-form-item>
+					<u-form-item @click="showHospital = true; hideKeyboard()" label="就诊医院" prop="userAge" borderBottom
+						label-position="top" label-width="100%" style="margin-top: 8px;">
+						<u-input v-model="formData.address" disabled disabledColor="#ffffff" placeholder="请选择就诊医院"
+							border="none"></u-input>
+						<u-icon slot="right" name="arrow-right"></u-icon>
 					</u-form-item>
 					<u-form-item label="请选择预约开始时间" prop="startTime" borderBottom label-position="top" label-width="100%"
 						style="margin-top: 8px;" @click="showStartTimeBoard()">
@@ -64,6 +78,11 @@
 			<u-action-sheet :show="showcontactWay" :actions="contactWayArray" title="请选择如何联系您"
 				@close="showcontactWay = false" @select="contactWaySelect">
 			</u-action-sheet>
+
+			<u-action-sheet class="scroll-view" :show="showHospital" :actions="hospitalList" title="请选择就诊医院"
+				@close="showHospital = false" @select="hospitalSelect">
+			</u-action-sheet>
+
 			<!-- 初始时间选择 -->
 			<u-datetime-picker :show="startTimeShow" :minDate="initTime" mode="datetime" :formatter="setFormatter"
 				@confirm="handleSelectStartTime" @cancel="this.startTimeShow = false"
@@ -80,7 +99,7 @@
 			<!-- 订单信息二次提示 -->
 			<u-modal :show="orderDetailShow" title="订单提示" confirmText="去支付" cancelText="取消" :showCancelButton="true"
 				@confirm="submit" @cancel="orderDetailShow = false">
-				<view>您在合肥市人民第一医院
+				<view>您在{{formData.address}}
 					<view>
 						<view>于{{ formData.startTime }}-{{ formData.endTime }}预定了时长为{{ duration }}小时</view>
 						<view>金额为{{ Number(totalPrice)/100 }}元的{{ isUprentScope ? '加急订单' : '预约订单' }}</view>
@@ -117,7 +136,9 @@
 					userDescription: '',
 					contact: 0,
 					risk_content: '',
-					address: '合肥市第一人民医院',
+					address: '',
+					hospitalId:''
+					
 				},
 				rules: {
 					userPhone: [{
@@ -144,6 +165,7 @@
 				},
 				city: '',
 				showcontactWay: false,
+				showHospital: false,
 				sexArray: [{
 						name: '男',
 						value: 1
@@ -187,15 +209,6 @@
 						value: 2
 					},
 				],
-				// 标记点
-				markers: [{
-					id: 1,
-					latitude: 31.8531187,
-					longitude: 117.2550177,
-					name: '',
-					width: '25',
-					height: '25',
-				}],
 				// 需求说明
 				requirementValue: '',
 				// 其他需求说明
@@ -220,17 +233,45 @@
 				// 跳转支付页面传递参数
 				message: {},
 				// 地图经度
-				latitude: '',
+				latitude: '31.8531187',
 				// 地图纬度
-				longitude: ''
+				longitude: '117.2550177',
+				// 附近医院列表
+				hospitalList: [],
+				// 当前选择的医院
+				currentHospital:null,
+				showCurrentHospital:false,
+				MapContext:null,
+				markers:[],
+				defaultMarker:{
+					id: 0,
+					latitude:31.8531187,
+					longitude:117.2550177,
+					width:20,
+					height:25,
+				}
 			}
 		},
 		onLoad() {
 			this.initTime = this.formatTimestampDelay(3)
+			this.getNearHospitalInfo(31.8531187,117.2550177)
 			this.getLocationInfo()
 		},
 		onReady() {
 			this.$refs.form.setRules(this.rules);
+		},
+		watch: {
+			// 监听经度维度变化
+			latitude: {
+				async handler(val) {
+					if(val){
+						this.$nextTick(()=>{
+							this.MapContext = wx.createMapContext('myMap')
+						})
+					}
+				},
+				immediate:true
+			}
 		},
 		computed: {
 			contactItem() {
@@ -239,9 +280,57 @@
 			},
 			endTimeDate() {
 				return this.initTime
+			},
+			hospitalItem() {
+				// const index = this.hospitalList.findIndex(item => item.value == this.formData.address)
+				// return this.hospitalList[index].name
+				return this.formData.address
 			}
 		},
 		methods: {
+			// 获取附近医院信息
+			async getNearHospitalInfo(latitude= 31.8531187,longitude = 117.2550177){
+				const detail = await request({
+					url: "api/get/near/hospital",
+					method: "GET",
+					data: {
+						latitude,
+						longitude,
+						highPrecision: false,
+					}
+				})
+				if (detail?.status == 1) {
+					this.orderDetailShow = false
+					this.$refs.uToast.show({
+						message: message,
+					})
+				}
+				this.hospitalList = detail.data.data.map(item => {
+					item.name = item.hospitalName
+					item.value = item.id
+					return item
+				})
+				this.markers = this.hospitalList.map(v=>{
+					return {
+						...v,
+						iconPath:'https://junzean.com.cn/images/hospital_market.png',
+						width:36,
+						height:36
+					}
+				})
+				this.markers.unshift(this.defaultMarker)
+				const index = this.markers.findIndex(f => f.markerId === 0)
+				const mark = {
+					markerId:0,
+					width:20,
+					height:25,
+					...detail
+				}
+				if(index >= 0){
+					this.markers.splice(index,1)
+				}
+				this.markers.unshift(mark)
+			},
 			// 手机号正则   1(3[0-9]|4[01456879]|5[0-35-9]|6[2567]|7[0-8]|8[0-9]|9[0-35-9])\d{8}
 			// 时间戳转日期
 			formatTimestamp(timestamp) {
@@ -268,18 +357,14 @@
 				uni.getLocation({
 					type: "gcj02",
 					success: function(data) {
-						let latitude = data.latitude;
-						let longitude = data.longitude;
-						that.latitude = latitude
-						that.longitude = longitude
-						that.markers = [{
-							id: 1,
-							latitude,
-							longitude,
-							iconPath: '../../../static/img/pos.png',
-							width: '20',
-							height: '20',
-						}]
+						// let latitude = data.latitude;
+						// let longitude = data.longitude;
+						// that.latitude = latitude
+						// that.longitude = longitude
+						let latitude =  31.8531187;
+						let longitude = 117.2550177;
+						that.latitude = 31.8531187
+						that.longitude = 117.2550177
 						that.circles = [{ //在地图上显示圆
 							latitude,
 							longitude,
@@ -291,47 +376,44 @@
 					}
 				});
 			},
-			chooseLocationInfo() {
-				let that = this;
-				uni.getLocation({
-					type: "gcj02",
-					success: function(data) {
-						let latitude = data.latitude;
-						let longitude = data.longitude;
-						// that.markers = [{
-						// 	id: 1,
-						// 	latitude,
-						// 	longitude,
-						// 	iconPath: '../../../static/img/pos.png',
-						// 	width: '20',
-						// 	height: '20',
-						// }]
-						// that.circles = [{ //在地图上显示圆
-						// 	latitude,
-						// 	longitude,
-						// 	fillColor: "#D9E6EF", //填充颜色
-						// 	color: "#A7B6CB", //描边的颜色
-						// 	radius: 50, //半径
-						// 	strokeWidth: 2 //描边的宽度
-						// }]
-						// 选择定位
-						uni.chooseLocation({
-							latitude,
-							longitude,
-							keyword:'医院',
-							success: function(res) {
-								that.place = res;
-								var add_data = that.addressard(res.address);
-								that.formData.positionData = {
-									...add_data,
-									latitude: res.latitude,
-									longitude: res.longitude
-								}
-							}
-						});
-					}
-				});
-			},
+			// chooseLocationInfo() {
+			// 	let that = this;
+			// 	uni.getLocation({
+			// 		type: "gcj02",
+			// 		success: async function(data) {
+			// 			// let latitude = data.latitude;
+			// 			// let longitude = data.longitude;
+			// 			let latitude = 31.8531187;
+			// 			let longitude = 117.2550177;
+			// 			that.latitude = 31.8531187
+			// 			that.longitude = 117.2550177
+			// 			// that.circles = [{ //在地图上显示圆
+			// 			// 	latitude,
+			// 			// 	longitude,
+			// 			// 	fillColor: "#D9E6EF", //填充颜色
+			// 			// 	color: "#A7B6CB", //描边的颜色
+			// 			// 	radius: 50, //半径
+			// 			// 	strokeWidth: 2 //描边的宽度
+			// 			// }]
+			// 			// 选择定位
+			// 			uni.chooseLocation({
+			// 				latitude,
+			// 				longitude,
+			// 				keyword: '医院',
+			// 				success: function(res) {
+			// 					that.place = res;
+			// 					var add_data = that.addressard(res.address);
+			// 					that.formData.positionData = {
+			// 						...add_data,
+			// 						latitude: res.latitude,
+			// 						longitude: res.longitude
+			// 					}
+			// 				}
+			// 			});
+			// 		}
+			// 	});
+			// },
+			
 			changeGender(val) {
 				this.formData.requireGender = val
 			},
@@ -342,6 +424,13 @@
 			contactWaySelect(e) {
 				this.formData.contact = e.value
 				this.$refs.form.validateField('formData.contactWay')
+			},
+			hospitalSelect(e) {
+				this.formData.address = e.name
+				this.latitude = e.latitude
+				this.longitude = e.longitude
+				console.log('this.formData.address',this.formData.address)
+				this.$refs.form.validateField('formData.address')
 			},
 			hideKeyboard() {
 				uni.hideKeyboard()
@@ -425,7 +514,9 @@
 				const formData = {
 					...this.formData,
 					startTime: this.formatTimestampString(this.formData.startTime),
-					endTime: this.formatTimestampString(this.formData.endTime)
+					endTime: this.formatTimestampString(this.formData.endTime),
+					longitude: this.longitude, // 经度
+					latitude: this.latitude // 维度
 				}
 				const res = await request({
 					url: 'api/create/order',
@@ -455,6 +546,31 @@
 					})
 				}
 			},
+			// 点击图标切换医院
+			handleTapMarker(e){
+				const { markerId } = e.detail
+				if(!markerId) return
+				const currentHos = this.hospitalList.find(v=>v.id === markerId)
+				if(currentHos){
+					const {latitude,longitude} = currentHos
+					this.MapContext.moveToLocation({
+						latitude,longitude
+					})
+					setTimeout(()=>{
+						this.currentHospital = currentHos
+						this.showCurrentHospital = true
+					},100)
+				}
+			},
+			// 点击地图
+			handleTapMap(e){
+				const {detail} = e
+				if(!detail) return
+				this.showCurrentHospital = false
+				this.MapContext.moveToLocation(detail)
+				const {longitude,latitude} = detail
+				this.getNearHospitalInfo(latitude,longitude)
+			}
 		}
 	}
 </script>
@@ -468,5 +584,46 @@
 				margin-left: 0 !important;
 			}
 		}
+			
+		.map-area{
+			position: relative;
+		}
+			
+		.current-hospital {
+			position: absolute;
+			left: 0;
+			bottom: 0;
+			z-index: 100;
+			padding: 10px 20px;
+			width: 100%;
+			height: 100px;
+			background: linear-gradient(to bottom,#fffefead,#ffffff);
+			box-sizing: border-box;
+				
+			.name {
+				font-weight: 600;
+				font-size: 24px;
+				color: #333333;
+				
+			}
+			.tags{
+				display: flex;
+				color: #ffffff;
+				margin-top: 10px;
+				.tag{
+					border-radius: 4px;
+					padding: 4px 8px;
+					font-size: 12px;
+					background-color: #4bbd03;
+					margin-right: 10px;
+				}
+			}
+			
+		}
+	}
+
+	/deep/.u-action-sheet__item-wrap {
+		max-height: 80vh !important;
+
 	}
 </style>
--- pages/pre_order/pre_order.vue
+++ pages/pre_order/pre_order.vue
@@ -1,7 +1,9 @@
 <template>
 	<view class="pre_order_page">
-		这是首页
-		<u-button type="primary" text="去下单" @click="goToOrder"></u-button>
+		<view  class="home-btn">
+			<u-button type="primary" plain="true" text="去下单"  @click="goToOrder"></u-button>
+		</view>
+		<image class="home-image" src="../../static/home/home.png"></image>
 	</view>
 </template>
 
@@ -22,13 +24,27 @@
 	}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 	.pre_order_page {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-		padding: 20px;
-		height: calc(100vh - 44px) !important;
+		height: 100vh!important;
+	} 
+	.home-image{
+		height: 100%;
+		width: 100%;
+	}
+	.home-btn{
+		padding: 0 20px;
+		position: absolute;
+		bottom: 30px;
+		width: 100%;
+		box-sizing: border-box;
+	}
+	/deep/.u-button--plain.u-button--primary{
+		color: #bb6664  !important;
+		border-color: #bb6664;
 	}
 </style>
